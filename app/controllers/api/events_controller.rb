75267cdc644ee0c7b0499c701754ef3d05c4b753
Nans Noel <nans.noel@gmail.com>
2021-06-04 20:46:09 +0200

    Add credit

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -2,22 +2,36 @@ class EventsController < ApplicationController
   before_action :authenticate_user!
 
   def receive_events
-    params =
-      {
-        user_id: "219053ce-c51a-11eb-8529-0242ac130003",
-        event: event,
-        points_earned: 0,
-        user: {
-          first_name:user.firstname,
-          email: user.email
-        }
-      }
-    if params[:event]
+
+    if params[:event] == "OPTIN"
       # Event 1 - OptIn
       # Retourner l'URL si success sinon ?
-    elsif params[:event]
-      # Event 2 - Création d'un crédit pour la partie
+      user = User.where(
+        fidmarques_uuid: params[:user_id],
+        email: params[:user][:email],
+        name: params[:user][:name]
+      ).first_or_create do |user|
+        user.password = SecureRandom.hex(10)
+      end
+
+      if user.present?
+        render json: { message: "http://goose-fidmarques.herokuapp.com/#{user.fidmarques_uuid}", status: 200}
+      else
+        render json: { message: "User not found or created", status: 403 }
+      end
+
+    elsif params[:event] == "EARN"
       # Retourner l'URL si success
+      if params[:points_earned].positive?
+        create_credit = Game::CreateCredit.run(fidmarques_uuid: params[:user_id])
+        if create_credit.success?
+          render json: { message: "http://goose-fidmarques.herokuapp.com/#{user.fidmarques_uuid}", status: 200}
+        else
+          render json: { message: "Credit could not be credited", status: 403 }
+        end
+      else
+
+      end
     else
       # Rien à faire donc je te dis NOP
     end
--- app/mutations/game/create_credit.rb
+++ app/mutations/game/create_credit.rb
@@ -1,11 +1,25 @@
 class Game::CreateCredit < Mutations::Command
 
   required do
-    model :user
+    string :fidmarques_uuid
+  end
+
+  def validate
+    @user = User.where(fidmarques_uuid: fidmarques_uuid).first
+    @game = @user.current_game
+    @players = @game.players
   end
 
   def execute
+    create_credit_to_all_users
+  end
 
+  def create_credit_to_all_users
+    credit = []
+    @players.each do |player|
+      credit << GameCredit.create!(game_id: @game.id, player_id: player.id, is_used: 0)
+    end
+    return true if credit.count == @players.count
   end
 
 end
