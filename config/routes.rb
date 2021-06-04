5d85019f601656037f65b40e34d0e74be2e04159
Nans Noel <nans.noel@gmail.com>
2021-06-04 21:51:11 +0200

    Fix root

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  root to: 'homepage#index'
+  get 'homepage#index/:uuid' => 'homepage#index'
+
+  # match "/patients/:id" => "patients#show"
   #
-  # root to: 'homepage#index', :id => '1'
-  get 'homepage#index/:uuid' => 'homepage#index', :as => :root
+  # get 'homepage#index/:uuid' => 'homepage#index', :as => :root
 
   resources :games
   resources :players
